5ab377c63082d4db54ab0e8ed5155601042edade
AshiqueULmustafa <ashiqueulmustafa94@gmail.com>
2024-10-31 00:32:00 +0600

    made the layout scrollable with scrollview as the component was not confining in the screen it was creating a problem

--- app/src/main/java/com/ashique/signuppage/SignUpPage.kt
+++ app/src/main/java/com/ashique/signuppage/SignUpPage.kt
@@ -2,6 +2,8 @@ package com.ashique.signuppage
 
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -12,8 +14,15 @@ class SignUpPage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
-
         setContentView(R.layout.activity_sign_up_page)
+        val fName: EditText = findViewById(R.id.etFname)
+        val lName: EditText = findViewById(R.id.etLname)
+        val dob: EditText = findViewById(R.id.etDOB)
+        val email: EditText = findViewById(R.id.etEmail)
+        val password: EditText = findViewById(R.id.etPass)
+        val phone: EditText = findViewById(R.id.etPhone)
+        val confirmpass: EditText = findViewById(R.id.etCpass)
+        val btnsignup : Button = findViewById(R.id.btnSubmit)
 
     }
 }
