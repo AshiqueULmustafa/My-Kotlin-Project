77f30616729e4b04cd61e579a242ad8dfff497e5
AshiqueULmustafa <ashiqueulmustafa94@gmail.com>
2024-10-30 22:41:30 +0600

    completed though app not working

--- app/src/main/java/com/ashique/signuppage/SignUpPage.kt
+++ app/src/main/java/com/ashique/signuppage/SignUpPage.kt
@@ -1,14 +1,18 @@
 package com.ashique.signuppage
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class SignUpPage : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
+        Log.d("mydebug","hello")
         setContentView(R.layout.activity_sign_up_page)
 
     }
