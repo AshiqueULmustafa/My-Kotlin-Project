d40c43a47e4454c1956ac5e9305940e8f4ac069c
AshiqueULmustafa <ashiqueulmustafa94@gmail.com>
2024-10-31 01:42:16 +0600

    added conditin to change the color of the button when all the information is given but it is not working

--- app/src/main/java/com/ashique/signuppage/SignUpPage.kt
+++ app/src/main/java/com/ashique/signuppage/SignUpPage.kt
@@ -1,28 +1,66 @@
 package com.ashique.signuppage
 
+
+import android.graphics.Color
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.EditText
-import androidx.activity.enableEdgeToEdge
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 
 class SignUpPage : AppCompatActivity() {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up_page)
-        val fName: EditText = findViewById(R.id.etFname)
-        val lName: EditText = findViewById(R.id.etLname)
-        val dob: EditText = findViewById(R.id.etDOB)
-        val email: EditText = findViewById(R.id.etEmail)
-        val password: EditText = findViewById(R.id.etPass)
-        val phone: EditText = findViewById(R.id.etPhone)
-        val confirmpass: EditText = findViewById(R.id.etCpass)
-        val btnsignup : Button = findViewById(R.id.btnSubmit)
+        val fName = findViewById<EditText>(R.id.etFname)
+        val lName = findViewById<EditText>(R.id.etLname)
+        val dob = findViewById<EditText>(R.id.etDOB)
+        val email = findViewById<EditText>(R.id.etEmail)
+        val password = findViewById<EditText>(R.id.etPass)
+        val phone = findViewById<EditText>(R.id.etPhone)
+        val confirmpass = findViewById<EditText>(R.id.etCpass)
+        val btnsubmit : Button = findViewById(R.id.btnSubmit)
+
+        var allFilled = false
+
+
+        if(fName.text.toString().trim().isNullOrEmpty() &&
+            lName.text.toString().trim().isNullOrEmpty() &&
+            dob.text.toString().trim().isNullOrEmpty() &&
+            email.text.toString().trim().isNullOrEmpty() &&
+            password.text.toString().trim().isNullOrEmpty() &&
+            phone.text.toString().trim().isNullOrEmpty() &&
+            confirmpass.text.toString().trim().isNullOrEmpty() &&
+            password.text.toString().trim().isNullOrEmpty()
+        ){}
+        else{
+            Log.d("mydebug", "hello")
+            allFilled = true
+            btnsubmit.setBackgroundColor(Color.CYAN)
+        }
+
+
+        if(allFilled) {
+            Log.d("mydebug", "hello")
+        }
+        btnsubmit.setOnClickListener(){ view->
+            if(allFilled){
+                Toast.makeText(this@SignUpPage,"Your Data is Accepted", Toast.LENGTH_LONG).show()
+            }
+            else{
+                Toast.makeText(this@SignUpPage,"Please Complete All The Information", Toast.LENGTH_SHORT).show()
+            }
+        }
+
 
     }
 }
