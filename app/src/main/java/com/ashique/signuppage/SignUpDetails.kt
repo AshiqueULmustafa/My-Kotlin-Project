6f429b1255d434f6fa25fd5c1623195919a7b0ec
AshiqueULmustafa <ashiqueulmustafa94@gmail.com>
2024-11-01 16:40:06 +0600

    created a new xml to view sign up details

---
+++ app/src/main/java/com/ashique/signuppage/SignUpDetails.kt
@@ -0,0 +1,12 @@
+package com.ashique.signuppage
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SignUpDetails : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up_details)
+
+    }
+}
