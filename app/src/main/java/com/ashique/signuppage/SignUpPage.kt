160adc111bc19aadd90addf6231927c262427f10
AshiqueULmustafa <ashiqueulmustafa94@gmail.com>
2024-11-15 01:17:54 +0600

    tried to optimize the button click listener by mapping the edittext to textview but it doesnt work

--- app/src/main/java/com/ashique/signuppage/SignUpPage.kt
+++ app/src/main/java/com/ashique/signuppage/SignUpPage.kt
@@ -1,18 +1,12 @@
 package com.ashique.signuppage
 
 
-import android.graphics.Color
 import android.os.Bundle
-import android.util.Log
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
-import android.widget.Toast
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 
 class SignUpPage : AppCompatActivity() {
 
@@ -22,46 +16,107 @@ class SignUpPage : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up_page)
 
-        val fName = findViewById<EditText>(R.id.etFname)
-        val lName = findViewById<EditText>(R.id.etLname)
-        val Dob = findViewById<EditText>(R.id.etDOB)
-        val Email = findViewById<EditText>(R.id.etEmail)
-        val Password = findViewById<EditText>(R.id.etPass)
-        val Phone = findViewById<EditText>(R.id.etPhone)
-        val Confirmpass = findViewById<EditText>(R.id.etCpass)
-        val btnsubmit : Button = findViewById(R.id.btnSubmit)
+        val etfname = findViewById<EditText>(R.id.etFname)
+        val tvfname = findViewById<TextView>(R.id.missfname)
+        val etlname = findViewById<EditText>(R.id.etLname)
+        val tvlname = findViewById<TextView>(R.id.misslname)
+        val etdob = findViewById<EditText>(R.id.etDOB)
+        val tvdob = findViewById<TextView>(R.id.missdob)
+        val etemail = findViewById<EditText>(R.id.etEmail)
+        val tvemail = findViewById<TextView>(R.id.missemail)
+        val etaddress = findViewById<EditText>(R.id.etAddress)
+        val tvaddress = findViewById<TextView>(R.id.missaddress)
+        val etpassword = findViewById<EditText>(R.id.etPass)
+        val tvpass = findViewById<TextView>(R.id.misspass)
+        val etphone = findViewById<EditText>(R.id.etPhone)
+        val tvphone = findViewById<TextView>(R.id.missphone)
+        val etconfirmPass = findViewById<EditText>(R.id.etCpass)
+        val tvcpass = findViewById<TextView>(R.id.misscpass)
+        val tvmissmatch = findViewById<TextView>(R.id.missmatchpass)
+        val btnsubmit: Button = findViewById(R.id.btnSubmit)
+        val badPass: TextView = findViewById(R.id.tvBadPass)
 
 
 
-        /*if(isAnyFieldEmpty){
-
+        etpassword.onFocusChangeListener = View.OnFocusChangeListener { _, hasFocus ->
+            if (hasFocus) {
+                tvpass.visibility = View.GONE
+                badPass.visibility = View.VISIBLE
+            } else {
+                badPass.visibility = View.GONE
+            }
         }
-        else{
-            Log.d("mydebug", "no null")
-            btnsubmit.setBackgroundColor(Color.CYAN)
-        }*/
 
-        btnsubmit.setOnClickListener(){ view->
-            val fname = fName.text.toString().trim()
-            val lname = lName.text.toString().trim()
-            val dob =  Dob.text.toString().trim()
-            val email = Email.text.toString().trim()
-            val password = Password.text.toString().trim()
-            val phone = Phone.text.toString().trim()
-            val confirmpass = Confirmpass.text.toString().trim()
 
-            val field = listOf(fname,lname,dob,email,password,confirmpass)
-            val isAnyFieldEmpty : Boolean = field.any{it.isNullOrEmpty()}
+        btnsubmit.setOnClickListener {
 
-            if(isAnyFieldEmpty
-                ){
-                Toast.makeText(this@SignUpPage,"Please Complete All The Information", Toast.LENGTH_SHORT).show()
-            }
-            else{
-                btnsubmit.setBackgroundColor(Color.CYAN)
-                Toast.makeText(this@SignUpPage,"Your Data is Accepted", Toast.LENGTH_LONG).show()
+            val fields = mapOf(
+                etfname to tvfname,
+                etlname to tvlname,
+                etdob to tvdob,
+                etaddress to tvaddress,
+                etemail to tvemail,
+                etphone to tvphone,
+                etconfirmPass to tvcpass
+            )
+            fields.forEach{ (editText, textview)->
+                val fieldvalue = editText.toString().trim()
+                if(fieldvalue.isEmpty()){
+                    textview.visibility = View.VISIBLE
+                }
+                else {
+                    textview.visibility = View.GONE
+                }
 
             }
+
+
+//                if(fname.isEmpty()){
+//                    tvfname.visibility = View.VISIBLE
+//                }
+//                if(lname.isEmpty()){
+//                    tvlname.visibility = View.VISIBLE
+//                }
+//                if(dob.isEmpty()){
+//                    tvdob.visibility = View.VISIBLE
+//                }
+//                if(email.isEmpty()){
+//                    tvemail.visibility = View.VISIBLE
+//                }
+
+
+            val password = etpassword.toString().trim()
+            val confirmpass = etconfirmPass.toString().trim()
+
+            if (password.isEmpty()) {
+                tvpass.visibility = View.VISIBLE
+            } else {
+                tvpass.visibility = View.GONE
+            }
+
+            if (confirmpass.isEmpty()) {
+                tvcpass.visibility = View.VISIBLE
+            } else if (password != confirmpass) {
+                tvmissmatch.visibility = View.VISIBLE
+            } else {
+                tvmissmatch.visibility = View.GONE
+            }
+
+
+//            val field = listOf(fname, lname, etdob, etemail,etphone, etpassword, etconfirmpass)
+//            val isAnyFieldEmpty: Boolean = field.any { it.isEmpty() }
+//            if (isAnyFieldEmpty
+//            ) {
+//                Toast.makeText(
+//                    this@SignUpPage,
+//                    "Please Complete All The Information",
+//                    Toast.LENGTH_SHORT
+//                ).show()
+//            } else {
+//                btnsubmit.setBackgroundColor(Color.CYAN)
+//                Toast.makeText(this@SignUpPage, "Your Data is Accepted", Toast.LENGTH_LONG).show()
+//
+//            }
         }
 
 
